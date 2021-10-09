802eb0c494a788bbed69833b64e27678859d3d1d
narfy <narfy695@gmail.com>
2021-10-09 19:52:44 +0200

    Ajout de tests dans putstring

--- nachos/code/test/putstring.c
+++ nachos/code/test/putstring.c
@@ -20,6 +20,28 @@ main()
     s[4]= '\0';
 
     print(s);
+    
+    //test on a buffer without a \0
+    /*char s[5];
+    s[0]= 't';
+    s[1]= 'e';
+    s[2]= 's';
+    s[3]= 't';
+    s[4]= 's';
+    print(s)*/
+
+    //test on a empty buffer
+    /*char s[5];
+    print(s);*/
+
+    //test on a buffer full of \0
+    /*char s[5];
+    s[0]= '\0';
+    s[1]= '\0';
+    s[2]= '\0';
+    s[3]= '\0';
+    s[4]= '\0';
+    print(s)*/
     Halt();
 }
 #endif //CHANGED
