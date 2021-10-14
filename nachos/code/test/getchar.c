f30c009ad0675e00fa4e79dbc5adaebf04f60d2b
MaxDumntl <maxime.pascal.dumonteil@gmail.com>
2021-10-14 23:42:34 +0200

    Ajout du commentaire d'explication de test de getchar

--- nachos/code/test/getchar.c
+++ nachos/code/test/getchar.c
@@ -2,10 +2,23 @@
 
 #include "syscall.h"
 
+/**
+ * Tests have to be done by user in the console
+ *
+ * - 1st test : enter a simple char like 'a' to test the simpliest way to use GetChar()
+ *              exceptation : print the char
+ * - 2nd test : enter without enter a char
+ *              exceptation : nothing is print
+ * - 3rd test : enter many char like a string
+ *              exceptation : show only the first char
+ * - 4th test : enter the EOF char using CRTL-D
+ *              exceptation : print EOF char because it's recognize as end of a file
+ * 
+ */
+
 void testGet()
 {
     int res = GetChar();
-
     PutChar(res);
 
 }
