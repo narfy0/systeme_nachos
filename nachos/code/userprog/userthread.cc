ab031e54ed2de845480bef15e19ee7bc3855fed4
MaxDumtl <maxime.pascal.dumonteil@gmail.com>
2021-10-18 15:45:25 +0200

    Correction StartUserThread I.5

--- nachos/code/userprog/userthread.cc
+++ nachos/code/userprog/userthread.cc
@@ -1,7 +1,7 @@
 #ifdef CHANGED
 
 #include "userthread.h"
-
+#include "threads.h"
 #include "machine.h"
 #include "addrspace.h"
 
@@ -14,31 +14,32 @@ static void StartUserThread(void *schmurtz){
     AddrSpace *space;
     Machine *machine;
 
-    int beginAddrStack = space->AllocateUserStack();
-    DEBUG('x', "Debug : StartUserThread, beginAddrStack %d\n", beginAddrStack);
-    
     int i;
     for (i = 0; i < NumTotalRegs; i++)
 	machine->WriteRegister (i, 0);
 
     // Initial program counter -- must be location of "Start"
-    machine->WriteRegister (PCReg, beginAddrStack);
+    Schmurtz tmp = *(Schmurtz *) schmurtz;
+    DEBUG('x', "Debug : StartUserThread, Schurmtz.f val%d\n", tmp.f);
+    machine->WriteRegister (PCReg, tmp.f);
 
     // Need to also tell MIPS where next instruction is, because
     // of branch delay possibility
     machine->WriteRegister (NextPCReg, machine->ReadRegister(PCReg) + 4);
 
-   
+
+    int beginAddrStack = space->AllocateUserStack();
+    DEBUG('x', "Debug : StartUserThread, beginAddrStack %d\n", beginAddrStack);
 }
 
 int do_ThreadCreate(int f, int arg){
+    DEBUG('x', "Debug : do_ThreadCreate, f = %d\n", f);
     Schmurtz *schmurtz;
     schmurtz->f = f;
     schmurtz->arg = arg;
 
-    //...
-
-    StartUserThread(schmurtz); 
+    //Thread *newThread;
+    //newThread->Start(StartUserThread, schmurtz);
 }
 
 void do_ThreadExit(){
