99bb241bae10f4bd84f050abc0072832d1c7befa
MaxDumtl <maxime.pascal.dumonteil@gmail.com>
2021-10-18 15:26:26 +0200

    Implementation de StartUserThread() pour initialiser les reegistres du thread + ajout des include I.5

--- nachos/code/userprog/.userthread.d
+++ nachos/code/userprog/.userthread.d
@@ -1,1 +1,6 @@
-.userthread.d userthread.o: userthread.cc userthread.h
+.userthread.d userthread.o: userthread.cc userthread.h ../machine/machine.h \
+ ../threads/copyright.h ../threads/utility.h ../threads/copyright.h \
+ ../threads/bool.h ../machine/sysdep.h ../machine/translate.h \
+ ../machine/disk.h addrspace.h ../filesys/filesys.h ../filesys/openfile.h \
+ ../machine/translate.h ../bin/noff.h ../threads/list.h \
+ ../threads/utility.h
--- nachos/code/userprog/userthread.cc
+++ nachos/code/userprog/userthread.cc
@@ -2,13 +2,33 @@
 
 #include "userthread.h"
 
+#include "machine.h"
+#include "addrspace.h"
+
 typedef struct {
     int f;
     int arg;
 } Schmurtz;
 
 static void StartUserThread(void *schmurtz){
+    AddrSpace *space;
+    Machine *machine;
 
+    int beginAddrStack = space->AllocateUserStack();
+    DEBUG('x', "Debug : StartUserThread, beginAddrStack %d\n", beginAddrStack);
+    
+    int i;
+    for (i = 0; i < NumTotalRegs; i++)
+	machine->WriteRegister (i, 0);
+
+    // Initial program counter -- must be location of "Start"
+    machine->WriteRegister (PCReg, beginAddrStack);
+
+    // Need to also tell MIPS where next instruction is, because
+    // of branch delay possibility
+    machine->WriteRegister (NextPCReg, machine->ReadRegister(PCReg) + 4);
+
+   
 }
 
 int do_ThreadCreate(int f, int arg){
