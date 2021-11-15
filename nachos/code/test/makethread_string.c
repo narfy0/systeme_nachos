c0e719e3944053b8b91da388091b55f43eb2e06f
MaxDumtl <maxime.pascal.dumonteil@gmail.com>
2021-11-15 10:59:31 +0100

    creation d'un fichier de test pour thread et putstring

---
+++ nachos/code/test/makethread_string.c
@@ -0,0 +1,43 @@
+#ifdef CHANGED
+
+#include "syscall.h"
+
+void printChar()
+{
+    char s[5];
+    
+    s[0]= 't';
+    s[1]= 'e';
+    s[2]= 's';
+    s[3]= 't';
+    s[4]= '\0';
+
+    PutString(s);
+    ThreadExit();
+}
+
+int 
+main()
+{
+    char s1[5];
+    
+    s1[0]= 'm';
+    s1[1]= 'a';
+    s1[2]= 'i';
+    s1[3]= 'n';
+    s1[4]= '\0';
+
+    ThreadCreate(printChar, 'a');
+    //ThreadCreate(printChar, 'b');
+    
+    //PutChar('&');
+   
+
+    PutString(s1);
+    
+    while(1){}
+
+    Halt();
+}
+
+#endif //CHANGED
