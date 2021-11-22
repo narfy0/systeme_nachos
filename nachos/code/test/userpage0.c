573f50598e3ad8d3956b4cdd961e6e26fd44b29a
Nahaki Barry <nabarry@emi.u-bordeaux.fr>
2021-11-22 14:33:45 +0100

    Création du premier fichier de test userpage0 (action I.1)

---
+++ nachos/code/test/userpage0.c
@@ -0,0 +1,17 @@
+#ifdef CHANGED
+
+#include "syscall.h"
+
+int 
+main()
+{
+    PutChar('a');
+    PutChar('b');
+    PutChar('c');
+    PutChar('d');
+    PutChar('e');
+    PutChar('f');
+    PutChar('g');
+}
+
+#endif //CHANGED
